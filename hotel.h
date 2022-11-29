f687ca2a1035f22593c43a6c415c70e015c90bc1
BaptisteLesquerre <116633910+BaptisteLesquerre@users.noreply.github.com>
2022-11-29 17:16:33 +0100

    Update hotel.h

--- hotel.h
+++ hotel.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 namespace hotel{
         class Hotel {
@@ -6,13 +7,13 @@ namespace hotel{
                 std::string _idhotel;
                 std::string _nom;
                 std::string _ville;
-                std::vector<chambre::Chambre> _tabchambres;
+                std::vector<hotel::Chambre> _tabchambres;
             public:
-                Hotel(std::string idhotel, std::string nom, std::string ville, std::vector<chambre::Chambre> tabchambres);
+                Hotel(std::string idhotel, std::string nom, std::string ville, std::vector<hotel::Chambre> tabchambres);
                 std::string idhotel() const;
                 std::string nom() const;
                 std::string ville() const;
-                std::vector<chambre::Chambre> tabchambre() const;
+                std::vector<hotel::Chambre> tabchambre() const;
                 void update_idhotel(std::string idhotel);
                 void update_nom(std::string nom);
                 void update_ville(std::string ville);
