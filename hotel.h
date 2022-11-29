702b0cef18d0422d3ad6c8a73262c88352ed0c6b
Baptiste Lesquerre-Caudebez <bl292327@ad.u-bourgogne.fr>
2022-11-29 17:06:59 +0100

    création de hotel.h

---
+++ hotel.h
@@ -0,0 +1,20 @@
+#include <iostream>
+
+namespace hotel{
+        class Hotel {
+            private:
+                std::string _idhotel;
+                std::string _nom;
+                std::string _ville;
+                std::vector<chambre::Chambre> _tabchambres;
+            public:
+                Hotel(std::string idhotel, std::string nom, std::string ville, std::vector<chambre::Chambre> tabchambres);
+                std::string idhotel() const;
+                std::string nom() const;
+                std::string ville() const;
+                std::vector<chambre::Chambre> tabchambre() const;
+                void update_idhotel(std::string idhotel);
+                void update_nom(std::string nom);
+                void update_ville(std::string ville);
+        }
+}
