b606806608d85d3e4a46a5d2ab39fbe3d08baa45
BaptisteLesquerre <116633910+BaptisteLesquerre@users.noreply.github.com>
2022-11-29 15:09:34 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "date.h"
+
+int main(int argc, char const *argv[]) {
+	date::Date d1(29,11,2022);
+	std::cout << d1.day() <<std::endl;
+	std::cout << d1.month() <<std::endl;
+	std::cout << d1.year() <<std::endl;
+	
+}
