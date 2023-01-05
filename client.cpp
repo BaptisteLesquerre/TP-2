5f576b311c657b080af4a097f0f6c6e41bf61776
BaptisteLesquerre <116633910+BaptisteLesquerre@users.noreply.github.com>
2023-01-05 08:35:27 +0100

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,29 +1,46 @@
 #include <iostream>
 #include "client.h"
 
-client::Client::Client(std::string idclient, std::string nom, std::string prenom):_idclient(idclient),_nom(nom),_prenom(prenom){
+Client::Client(std::string idclient, std::string nom, std::string prenom):_idclient(idclient),_nom(nom),_prenom(prenom){
 }
 
-std::string client::Client::idclient() const {
+std::string Client::idclient() const {
     return _idclient;
 }
 
-std::string client::Client::nom() const {
+std::string Client::nom() const {
     return _nom;
 }
 
-std::string client::Client::prenom() const {
+std::string Client::prenom() const {
     return _prenom;
 }
 
-void client::Client::update_idclient(std::string idclient){
+void Client::update_idclient(std::string idclient){
     _idclient = idclient;
 }
 
-void client::Client::update_nom(std::string nom){
+void Client::update_nom(std::string nom){
     _nom = nom;
 }
 
-void client::Client::update_prenom(std::string prenom){
+void Client::update_prenom(std::string prenom){
     _prenom = prenom;
 }
+
+std::string Client::tostring() const {
+    return "idclient : " + _idclient + " " + "nom : " + _nom + " " +"prenom : " + _prenom;
+}
+
+std::string Client::display(std::vector<Client> c1){
+    std::string c ="|";
+    for (int i = 0; i < c1.size(); i++){
+        c = c + c1[i].tostring() + "|";
+    }
+    return c;
+}
+
+std::ostream& operator<<(std::ostream& os,const Client& client){
+        os << client.tostring() <<std::endl;
+        return os;
+    }
