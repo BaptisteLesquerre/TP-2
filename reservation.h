0785a37a2ec88661bdede3135bb83efebc3c6408
BaptisteLesquerre <116633910+BaptisteLesquerre@users.noreply.github.com>
2023-01-05 14:32:51 +0100

    Update reservation.h
    update après avoir débuté la question 7

--- reservation.h
+++ reservation.h
@@ -1,13 +1,13 @@
 #include "date.h"
-
+#include "hotel.h"
 #include<iostream>
 #include<string>
 
 
-class reservation
+class Reservation
 {
 private:
-    date::Date _nuit_debut;
+    Date _nuit_debut;
     int _nb_nuits;
     int _numero_chambre;
     int _montant;
@@ -15,19 +15,22 @@ private:
     std::string _id_client;
 
 public:
-    reservation(date::Date nuit_debut, int nb_nuit, std::string id_hotel, int numero_chambre, std::string id_client, int montant);
+    Reservation(Date nuit_debut, int nb_nuit =0, std::string id_hotel = "", int numero_chambre =0, std::string id_client ="", int montant=0);
 
-    date::Date GetNuitDebut();
-    int GetNbNuits();
-    int GetNumeroChambre();
-    int GetMontant();
-    std::string GetIdHotel();
-    std::string GetIdClient();
+    Date getNuitDebut();
+    int getNbNuits();
+    int getNumeroChambre();
+    int getMontant();
+    std::string getIdHotel();
+    std::string getIdClient();
 
-    void SetNuitDebut(date::Date nuit_debut);
-    void SetNbNuits(int nb_nuits);
-    void SetNumeroChambre(int numero_chambre);
-    void SetMontant(int montant);
-    void SetIdHotel(std::string id_hotel);
-    void SetIdClient(std::string id_client);
+    void setNuitDebut(Date nuit_debut);
+    void setNbNuits(int nb_nuits);
+    void setNumeroChambre(int numero_chambre);
+    void setMontant(int montant);
+    void setIdHotel(std::string id_hotel);
+    void setIdClient(std::string id_client);
 };
+
+void saisi_reservation(std::vector<Reservation>& reservationtab,Reservation& r,hotel::Hotel h1,std::string idclient);
+bool rempty(std::vector<Reservation>& reservationtab, hotel::Hotel h1);
