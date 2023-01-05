53bc2f2aff1a75807de7b700cc4dabb5a5d84492
BaptisteLesquerre <116633910+BaptisteLesquerre@users.noreply.github.com>
2023-01-05 14:28:53 +0100

    Update date.cpp

--- date.cpp
+++ date.cpp
@@ -1,13 +1,12 @@
-#include <iostream>
 #include "date.h"
 #include <assert.h>
 
-date::Date::Date(int day, int month, int year):_day(day),_month(month),_year(year){
+Date::Date(int day, int month, int year):_day(day),_month(month),_year(year){
     bool status = isDate(day, month, year);
     assert(status && "Date is not valid");
 }
 
-bool date::Date::isDate(int day,int month,int year) {
+bool isDate(int day, int month, int year) {
     if ((day < 1 ) || (day> 31)) return false;
     if ((month <1) || (month>12)) return false;
     if (((month == 4) || (month == 6) || (month == 9) || (month == 11)) && (day > 30)) return false;
@@ -15,32 +14,32 @@ bool date::Date::isDate(int day,int month,int year) {
     return true;
 }
 
-void date::Date::updateday(int day){
+void Date::updateday(int day){
     bool status = isDate(day,_month,_year);
     assert(status && "Date is not valid");
     _day = day;
 }
 
-void date::Date::updatemonth(int month){
+void Date::updatemonth(int month){
     bool status = isDate(_day,month,_year);
     assert(status && "Date is not valid");
     _month = month;
 }
 
-void date::Date::updateyear(int year){
+void Date::updateyear(int year){
     bool status = isDate(_day,_month,year);
     assert(status && "Date is not valid");
     _year = year;
 }
 
-int date::Date::month() const{
+int Date::month() const{
      return _month;
 }
 
-int date::Date::day() const{
+int Date::day() const{
      return _day;
 }
 
-int date::Date::year() const{
+int Date::year() const{
      return _year;
 }
