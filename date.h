cec31fe23b3be6efb70a4b0681f435e0bd4914f0
BaptisteLesquerre <116633910+BaptisteLesquerre@users.noreply.github.com>
2023-01-12 23:05:04 +0100

    Update date.h

--- date.h
+++ date.h
@@ -1,23 +1,43 @@
+#include <iostream>
+
 #ifndef DATE_H
 #define DATE_H
 
-#include <iostream>
-
-	class Date {
-	 public:
-	  Date(int day =1, int month =1, int year =1);
-	  int month() const;
-	  int day() const;
+   class Date {
+   public:
+      Date(int day=1, int month=1, int year = 1);
+      int month() const;
+      int day() const;
 	  int year() const;
-	  void updateday(int day);
-	  void updatemonth(int month);
-	  void updateyear(int year);
+      void updateMonth(int month);
+      void updateDay(int day);
+	  void updateYear(int year);
+      void next();
+      void back();
 
-	 private:
-	  int _day;
-	  int _month;
-	  int _year;
-	};
-	bool isDate(int day, int month, int year);
+      bool operator == (const Date& d) const; // d1 == d2
+      bool operator != (const Date& d) const; // d1 != d2
+      bool operator < (const Date& d) const;  // d1 <  d2
+      bool operator > (const Date& d) const;  // d1 >  d2
+      bool operator <= (const Date& d) const; // d1 <= d2
+      bool operator >= (const Date& d) const; // d1 <= d2
+      Date operator + (const int days) const; // d1 + integer
+      Date operator - (const int days) const; // d1 - integer
+      Date operator ++ (); // prefix increment: ++date
+      Date operator -- (); // prefix decrement: --date
+      Date operator ++ (int); // use int paramater for postfix increment: date++
+      Date operator -- (int); // use int paramater for postfix decrement: date--
 
-#endif \\DATE_H
+   private:
+      int _month;
+      int _day;
+      int _year;
+
+   };
+
+   bool isDate(int day, int month, int year);
+   int getDaysInMonth(int month);
+   int dayOfYear(Date d);
+   std::string toString(Date d);
+
+#endif // DATE_H
